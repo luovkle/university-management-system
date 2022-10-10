088e3a8af7220a5fa8062562dd3237caf1095c89
luovkle <roberto.silva.dev@protonmail.com>
2022-10-10 17:39:09 -0500

    crud/post.py module added

--- backend/app/app/api/api_v1/endpoints/posts.py
+++ backend/app/app/api/api_v1/endpoints/posts.py
@@ -1,19 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Query
-from sqlmodel import Session, select
+from fastapi import APIRouter, Depends, Query
+from sqlmodel import Session
 
 from app.api.deps import get_session
-from app.models import Post, PostCreate, PostRead, PostReadWithComments, PostUpdate
+from app.models import PostCreate, PostRead, PostReadWithComments, PostUpdate
 from app.utils import Tag, Prefix
+from app.crud.post import crud_post
 
 router = APIRouter(prefix=Prefix.posts, tags=[Tag.posts])
 
 
 @router.post("", response_model=PostRead, status_code=201)
 def create_post(*, session: Session = Depends(get_session), post: PostCreate):
-    db_post = Post.from_orm(post)
-    session.add(db_post)
-    session.commit()
-    session.refresh(db_post)
+    db_post = crud_post.create(session, post)
     return db_post
 
 
@@ -24,34 +22,17 @@ def read_posts(
     offset: int = 0,
     limit: int = Query(default=100, le=100),
 ):
-    posts = session.exec(select(Post).offset(offset).limit(limit)).all()
-    return posts
+    db_posts = crud_post.read_many(session, offset, limit)
+    return db_posts
 
 
 @router.get("/{id}", response_model=PostReadWithComments)
 def read_post(*, session: Session = Depends(get_session), id: int):
-    post = session.get(Post, id)
-    if not post:
-        raise HTTPException(status_code=404)
-    return post
+    db_post = crud_post.read_single(session, id)
+    return db_post
 
 
 @router.put("/{id}", response_model=PostRead)
 def update_post(*, session: Session = Depends(get_session), post: PostUpdate, id: int):
-    existing_post = session.exec(select(Post).where(Post.title == post.title)).first()
-    if existing_post:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Title not available"
-        )
-    db_post = session.get(Post, id)
-    if not db_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
-        )
-    post_data = post.dict(exclude_unset=True)
-    for key, value in post_data.items():
-        setattr(db_post, key, value)
-    session.add(db_post)
-    session.commit()
-    session.refresh(db_post)
+    db_post = crud_post.update(session, post, id)
     return db_post
---
+++ backend/app/app/crud/post.py
@@ -0,0 +1,52 @@
+from fastapi import HTTPException, status
+from sqlmodel import Session, select
+
+from app.models import Post, PostCreate, PostUpdate
+
+
+class CRUDPost:
+    def get_by_title(self, session: Session, title: str):
+        db_post = session.exec(select(Post).where(Post.title == title)).first()
+        return db_post
+
+    def create(self, session: Session, post: PostCreate):
+        if self.get_by_title(session, post.title):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Title not available"
+            )
+        db_post = Post.from_orm(post)
+        session.add(db_post)
+        session.commit()
+        session.refresh(db_post)
+        return db_post
+
+    def read_many(self, session: Session, offset: int, limit: int):
+        db_posts = session.exec(select(Post).offset(offset).limit(limit)).all()
+        return db_posts
+
+    def read_single(self, session: Session, id: int):
+        db_post = session.get(Post, id)
+        if not db_post:
+            raise HTTPException(status_code=404, detail="Post not found")
+        return db_post
+
+    def update(self, session: Session, post: PostUpdate, id: int):
+        if post.title and self.get_by_title(session, post.title):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Title not available"
+            )
+        db_post = session.get(Post, id)
+        if not db_post:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+            )
+        post_data = post.dict(exclude_unset=True)
+        for key, value in post_data.items():
+            setattr(db_post, key, value)
+        session.add(db_post)
+        session.commit()
+        session.refresh(db_post)
+        return db_post
+
+
+crud_post = CRUDPost()
