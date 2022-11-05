bb060cae84c794d8fc554e37a72694df96cf76f0
luovkle <roberto.silva.dev@protonmail.com>
2022-11-05 15:19:00 -0600

    authentication is added to tasks endpoint

--- backend/app/app/api/api_v1/endpoints/tasks.py
+++ backend/app/app/api/api_v1/endpoints/tasks.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from app.api.deps import get_current_user
 from app.utils import Prefix, Tag
 from app.celery_worker import celery_app
+from app.models import User
 from app.models.task import TaskRead
 
 router = APIRouter(prefix=Prefix.tasks, tags=[Tag.tasks])
 
 
 @router.get("/{id}", response_model=TaskRead)
-def read_task(id: str):
+def read_task(*, current_user: User = Depends(get_current_user), id: str):
     result = celery_app.AsyncResult(id)
     return {
         "task_id": result.id,
